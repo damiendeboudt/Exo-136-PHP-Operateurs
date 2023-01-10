0c187e5f5f88d399c4da5706197eb94c8702a251
Damien <damien.deboudt@sfr.fr>
2023-01-10 14:54:04 +0100

    ok

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
 
 // Premiere ligne
-echo 10 5;
+echo 10 + 5 . '<br>';
 
 //Deuxieme ligne
-echo 10 2;
+echo 10 / 2 . '<br>';
 
 //Troisieme ligne
-if($a $b) {
-    echo "<br><br>a est identique à b";
+$a = 10;
+$b = 20;
+if($a === $b) {
+    echo "<br><br>a est identique à b" . '<br>';
 }
 
 //Quatrieme ligne
-if($a $b) {
-    echo"<br><br>a n'est pas identique à b";
+if($a !== $b) {
+    echo"<br><br>a n'est pas identique à b" . '<br>';
 }
 
 
 //Cinquieme ligne
 $arr1 = ["machin", "truc"];
 $arr2 = ["truc", "machin"];
-if($arr1 $arr2) {
-    echo "<br><br>les tableaux ont le même contenu";
+if($arr1 ===  $arr2) {
+    echo "<br><br>les tableaux ont le même contenu" . '<br>';
 }
 
